ac37178b44c0e3c199a3d15225484d7dabdd85e7
Aleksa Cvetkovic <aleksacvele3@gmail.com>
2021-02-04 14:19:10 +0100

    dodavanje entiteta1

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseConfiguration } from 'config/database.configuration';
 import { AppController } from './app.controller';
 import { Administrator } from './entities/administrator.entity';
+import { Feature } from './entities/feature.entity';
+import { Photo } from './entities/photo.entity';
+import { Reservation } from './entities/reservation.entity';
+import { Room } from './entities/room.entity';
+import { RoomFeature } from './entities/roomFeature.entity';
+import { RoomPrice } from './entities/roomPrice.entity';
+import { User } from './entities/user.entity';
 import { AdministratorService } from './services/administrator/administrator.service';
 
 @Module({
@@ -14,7 +21,7 @@ import { AdministratorService } from './services/administrator/administrator.ser
       username: DatabaseConfiguration.username,
       password: DatabaseConfiguration.password,
       database: DatabaseConfiguration.database,
-      entities: [Administrator],
+      entities: [Administrator,Feature,RoomFeature,Photo,User,Reservation,Room,RoomPrice],
     }),
     TypeOrmModule.forFeature([Administrator]),
   ],
